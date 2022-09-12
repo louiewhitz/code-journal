397b61779275504c41db464810577b2c17dfbf0f
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-09-12 19:39:19 +0000

    Issue 1 completed - in need of review

--- js/data.js
+++ js/data.js
@@ -1,8 +1,18 @@
 /* exported data */
-
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
+var previousEntryJSON = localStorage.getItem('data');
+if (previousEntryJSON !== null) {
+  data = JSON.parse(previousEntryJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnload);
+function beforeUnload() {
+  var jsonData = JSON.stringify(data);
+  localStorage.setItem('data', jsonData);
+}
---
+++ js/main.js
@@ -0,0 +1,34 @@
+var form = document.querySelector('#form');
+var imgSrc = document.querySelector('.photo-url');
+var placeholder = document.querySelector('#placeholder');
+
+// addEventListeners
+imgSrc.addEventListener('input', changeSrc);
+form.addEventListener('submit', handleSubmit);
+
+// functions for listener events
+function changeSrc(event) {
+  if (placeholder === '') {
+    imgSrc.setAttribute('src', '/images/placeholder-image-square.jpg');
+  } else {
+    placeholder.setAttribute('src', imgSrc.value);
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var title = document.querySelector('#title').value;
+  var photoUrl = document.querySelector('#photo').value;
+  var notes = document.querySelector('#notes').value;
+
+  var entry = {
+    title,
+    photoUrl,
+    notes,
+    entryId: data.nextEntryId
+  };
+  data.entries.push(entry);
+  data.nextEntryId++;
+  form.reset();
+  placeholder.setAttribute('src', '/images/placeholder-image-square.jpg');
+}
